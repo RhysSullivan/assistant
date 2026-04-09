island5sho/executor

derive SDK types from core and expose plugin store options
Add PromisifyService utility type to derive promise-based SDK interfaces
from core Effect types, reducing duplication across ToolRegistry,
SourceRegistry, SecretStore, PolicyEngine, SourceManager, and
SecretProvider. Use Pick for Executor sub-types.

Expose operationStore/bindingStore options on openapi, mcp, graphql,
and google-discovery plugin factories.

packages/plugins/google-discovery/src/promise.ts
@@ -9,5 +9,12 @@ export type {
   GoogleDiscoveryOAuthAuthResult,
 } from "./sdk/plugin";
 
-export const googleDiscoveryPlugin = (options?: {}) =>
-  _googleDiscoveryPlugin(options);
+export type { GoogleDiscoveryBindingStore } from "./sdk/binding-store";
+
+export interface GoogleDiscoveryPluginOptions {
+  readonly bindingStore?: import("./sdk/binding-store").GoogleDiscoveryBindingStore;
+}
+
+export const googleDiscoveryPlugin = (
+  options?: GoogleDiscoveryPluginOptions,
+) => _googleDiscoveryPlugin(options);

packages/plugins/graphql/src/promise.ts
@@ -2,5 +2,11 @@ import { graphqlPlugin as _graphqlPlugin } from "./sdk/plugin";
 
 export type { GraphqlSourceConfig } from "./sdk/plugin";
 export type { HeaderValue } from "./sdk/types";
+export type { GraphqlOperationStore } from "./sdk/operation-store";
 
-export const graphqlPlugin = (options?: {}) => _graphqlPlugin(options);
+export interface GraphqlPluginOptions {
+  readonly operationStore?: import("./sdk/operation-store").GraphqlOperationStore;
+}
+
+export const graphqlPlugin = (options?: GraphqlPluginOptions) =>
+  _graphqlPlugin(options);

packages/plugins/openapi/src/promise.ts
@@ -1,5 +1,11 @@
 import { openApiPlugin as _openApiPlugin } from "./sdk/plugin";
 
 export type { OpenApiSpecConfig } from "./sdk/plugin";
+export type { OpenApiOperationStore } from "./sdk/operation-store";
 
-export const openApiPlugin = (options?: {}) => _openApiPlugin(options);
+export interface OpenApiPluginOptions {
+  readonly operationStore?: import("./sdk/operation-store").OpenApiOperationStore;
+}
+
+export const openApiPlugin = (options?: OpenApiPluginOptions) =>
+  _openApiPlugin(options);

RhysSullivan/assistant

packages/plugins/google-discovery/src/promise.ts
@@ -9,5 +9,12 @@ export type {
   GoogleDiscoveryOAuthAuthResult,
 } from "./sdk/plugin";
 
-export const googleDiscoveryPlugin = (options?: {}) =>
-  googleDiscoveryPluginEffect(options);
+export type { GoogleDiscoveryBindingStore } from "./sdk/binding-store";
+
+export interface GoogleDiscoveryPluginOptions {
+  readonly bindingStore?: import("./sdk/binding-store").GoogleDiscoveryBindingStore;
+}
+
+export const googleDiscoveryPlugin = (
+  options?: GoogleDiscoveryPluginOptions,
+) => googleDiscoveryPluginEffect(options);

packages/plugins/graphql/src/promise.ts
@@ -2,5 +2,11 @@ import { graphqlPlugin as graphqlPluginEffect } from "./sdk/plugin";
 
 export type { GraphqlSourceConfig } from "./sdk/plugin";
 export type { HeaderValue } from "./sdk/types";
+export type { GraphqlOperationStore } from "./sdk/operation-store";
 
-export const graphqlPlugin = (options?: {}) => graphqlPluginEffect(options);
+export interface GraphqlPluginOptions {
+  readonly operationStore?: import("./sdk/operation-store").GraphqlOperationStore;
+}
+
+export const graphqlPlugin = (options?: GraphqlPluginOptions) =>
+  graphqlPluginEffect(options);

packages/plugins/openapi/src/promise.ts
@@ -1,5 +1,11 @@
 import { openApiPlugin as openApiPluginEffect } from "./sdk/plugin";
 
 export type { OpenApiSpecConfig } from "./sdk/plugin";
+export type { OpenApiOperationStore } from "./sdk/operation-store";
 
-export const openApiPlugin = (options?: {}) => openApiPluginEffect(options);
+export interface OpenApiPluginOptions {
+  readonly operationStore?: import("./sdk/operation-store").OpenApiOperationStore;
+}
+
+export const openApiPlugin = (options?: OpenApiPluginOptions) =>
+  openApiPluginEffect(options);